e4d271f05813e0df9ea1745fcd069e434a6c423c
ManuelSoft™ <46997457+manuelstaffa@users.noreply.github.com>
2023-05-11 22:05:51 +0800

    Create rpc_classifier.py
    init

---
+++ rpc_classifier/rpc_classifier.py
@@ -0,0 +1,27 @@
+import math
+import cv2
+import configparser
+import numpy as np
+import mediapipe as mp
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands
+
+
+def main():
+    # load config file
+    config = configparser.ConfigParser()
+    config.sections()
+    return
+
+
+def getCameraImage():
+    return
+
+
+def getHandLandmarks():
+    return
+
+
+if __name__ == '__main__':
+    main()
