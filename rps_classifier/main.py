af44fbe1f6162bc809681719320d96adeb3dc385
ManuelSoft™ <46997457+manuelstaffa@users.noreply.github.com>
2023-05-30 14:24:45 +0800

    removed unused import

--- rps_classifier/main.py
+++ rps_classifier/main.py
@@ -5,7 +5,6 @@ from rps_classifier import *
 import time
 import cv2
 import configparser
-import numpy as np
 import mediapipe as mp
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
